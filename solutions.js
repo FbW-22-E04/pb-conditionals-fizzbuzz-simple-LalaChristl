f15a7d2e912fdc6b7b2cb74e4ec0a2af60c0e1ae
LalaChristl <lesleyannchristl@gmail.com>
2022-07-12 11:54:11 +0200

    add JS

---
+++ solutions.js
@@ -0,0 +1,14 @@
+let num1 = 45;
+
+if (num1 % 3 === 0 && num1 % 5 === 0) {
+    console.log("FizzBuzz");
+}
+else if 
+(num1 % 3 === 0) {
+  console.log("Fizz");
+} else if (num1 % 5 === 0) {
+  console.log("Buzz");
+} 
+else {
+  console.log(num1);
+}
